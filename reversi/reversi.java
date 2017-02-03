cbfae446a143211bdf045f9f56c43c6f821cec0e
GifflarN <g1ffel@hotmail.com>
2017-02-03 15:49:26 +0100

    derp

--- reversi/reversi.java
+++ reversi/reversi.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class reversi {
-	static Boolean[][] board;
+	static String[][] board;
 	static Scanner s;
 	private static final int size = 8;
-	private static final Boolean WHITE = Boolean.TRUE, BLACK = Boolean.FALSE;
 
 	public static void main(String[] args) {
-		board = new Boolean[size][size];
+		board = new String[size][size];
 		setUp();
 		draw();
 		startGame();
@@ -43,9 +42,10 @@ public class reversi {
 					|| !Pattern.matches("[1-8]", inputs[1])) {
 				System.out.println("Faulty input, try again");
 			} else {
-				inputB = inputs[0].toLowerCase().charAt(0) - 'a' + 1;
-				inputA = Integer.parseInt(inputs[1]);
-				if (isLegalMove(inputA, inputB, BLACK, WHITE)) {
+				inputB = inputs[0].toLowerCase().charAt(0) - 'a';
+				inputA = Integer.parseInt(inputs[1])-1;
+				Coords coord = new Coords(inputA, inputB);
+				if (isLegalMove(coord, "BLACK", "WHITE")) {
 					accepted = true;
 				} else {
 					System.out.println("Illegal move, try again");
@@ -53,37 +53,49 @@ public class reversi {
 			}
 		}
 
-		board[inputA-1][inputB-1] = WHITE;
+		board[inputA][inputB] = "WHITE";
 
 	}
 
-	private static boolean isLegalMove(int inputA, int inputB, Boolean opponentCol, Boolean col) {
-		if (!(board[inputA + 1][inputB] == opponentCol
-				|| board[inputA + 1][inputB + 1] == opponentCol
-				|| board[inputA + 1][inputB - 1] == opponentCol
-				|| board[inputA - 1][inputB] == opponentCol
-				|| board[inputA - 1][inputB + 1] == opponentCol
-				|| board[inputA - 1][inputB - 1] == opponentCol
-				|| board[inputA][inputB + 1] == opponentCol || board[inputA][inputB - 1] == opponentCol)) {
+	private static boolean isLegalMove(Coords coord, String opponentCol, String col) {
+		if(!board[coord.x][coord.y].equals("EMPTY")){
 			return false;
 		}
-//		int counter = 0;
-//		for(int i = 0; i < size; i++){
-//			if(board[inputA][i] == col){
-//				counter+= Math.abs(inputA-i)-1;
-//			}
-//		}
+		checkAxis(coord.x+1, coord.y, opponentCol, col, "E");
+		checkAxis(coord.x+1, coord.y+1, opponentCol, col, "SE");
+		checkAxis(coord.x, coord.y+1, opponentCol, col, "S");
+		checkAxis(coord.x-1, coord.y+1, opponentCol, col, "SW");
+		checkAxis(coord.x-1, coord.y, opponentCol, col, "W");
+		checkAxis(coord.x-1, coord.y-1, opponentCol, col, "NW");
+		checkAxis(coord.x, coord.y-1, opponentCol, col, "N");
+		checkAxis(coord.x+1, coord.y, opponentCol, col, "NE");
+		
 		return true;
 
 	}
 
-	private static void draw() {
-		try {
-			Runtime.getRuntime().exec("cls");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	private static boolean checkAxis(int x, int y, String opponentCol,
+			String col, String direction) {
+		if(board[x][y].equals("EMPTY") || x == 8 || x == -1 || y == 8 || y == -1){
+			return false;
+		} else if(board[x][y].equals(col)){
+			return true;
 		}
+		switch(direction){
+		case("E"): return board[x][y] == opponentCol && checkAxis(x+1, y, opponentCol, col, "E");
+		case("SE"): return board[x][y] == opponentCol && checkAxis(x+1, y+1, opponentCol, col, "E");
+		case("S"): return board[x][y] == opponentCol && checkAxis(x, y+1, opponentCol, col, "E");
+		case("SW"): return board[x][y] == opponentCol && checkAxis(x-1, y+1, opponentCol, col, "E");
+		case("W"): return board[x][y] == opponentCol && checkAxis(x-1, y, opponentCol, col, "E");
+		case("NW"): return board[x][y] == opponentCol && checkAxis(x-1, y-1, opponentCol, col, "E");
+		case("N"): return board[x][y] == opponentCol && checkAxis(x, y-1, opponentCol, col, "E");
+		case("NE"): return board[x][y] == opponentCol && checkAxis(x+1, y-1, opponentCol, col, "E");
+		}
+		return false;
+		
+	}
+
+	private static void draw() {
 		for (int i = -1; i < size; i++) {
 			if (i == -1) {
 				System.out.print("  | a | b | c | d | e | f | g | h |\n");
@@ -93,14 +105,13 @@ public class reversi {
 				if (j == 0) {
 					System.out.print((i + 1) + " |");
 				}
-				if (board[i][j] == null) {
+				if (board[i][j].equals("EMPTY")) {
 					System.out.print("   |");
-				} else if (board[i][j].equals(WHITE)) {
+				} else if (board[i][j].equals("WHITE")) {
 					System.out.print(" W |");
-				} else if (board[i][j].equals(BLACK)) {
+				} else if (board[i][j].equals("BLACK")) {
 					System.out.print(" B |");
-				} else {
-				}
+				} 
 			}
 			System.out.println("");
 		}
@@ -110,12 +121,15 @@ public class reversi {
 	private static void setUp() {
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
-				board[i][j] = null;
+				board[i][j] = "EMPTY";
 			}
 		}
-		board[3][3] = WHITE;
-		board[4][4] = WHITE;
-		board[3][4] = BLACK;
-		board[4][3] = BLACK;
+		board[3][3] = "WHITE";
+		board[4][4] = "WHITE";
+		board[3][4] = "BLACK";
+		board[4][3] = "BLACK";
 	}
+	
+	
 }
+
